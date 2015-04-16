23c3f42c564c577f405c4a8ad91a291cd8f9c852
Josh O Farrell <hurleyjosh94@gmail.com>
2015-04-16 01:04:39 +0100

    Resposition
    Added jquery.ui file
    made div square, repositioned square & buttons

--- dom builder.js
+++ dom builder.js
@@ -1,1 +1,6 @@
 // JavaScript Document
+$(document).ready(function() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+});
