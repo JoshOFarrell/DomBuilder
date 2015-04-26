4ae039ae9ad80affdf78fffee4f482c4aa9857a0
Josh O Farrell <hurleyjosh94@gmail.com>
2015-04-26 04:43:54 +0100

    restart project
    files missing, restarting project

--- dom builder.js
+++ dom builder.js
@@ -1,6 +1,12 @@
 // JavaScript Document
-$(document).ready(function() {
+/*$(document).ready(function() {
     var table = document.getElementById("myTable");
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
+});*/
+
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideToggle("slow");
+    });
 });
